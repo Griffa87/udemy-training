5d56418a196b1bc730df03b527e2b19947c2ff73
Wayne Griffin <78621165+Griffa87@users.noreply.github.com>
2021-07-14 20:08:24 +0100

    Existing exercises added via upload.
    Tasks completed as per Udemy course.

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            //String interpolation is used to parse variables into console output
+            Console.WriteLine($"{name} is {age} years old");
+        }
+    }
+}
