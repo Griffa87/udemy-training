c47e95ceb096ff812d26718f8dc8c693b103fa97
Wayne Griffin <78621165+Griffa87@users.noreply.github.com>
2021-07-20 23:49:57 +0100

    Additional exercises added
    Progression made through the course. New files added

---
+++ SimpleMenu/SimpleMenu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SimpleMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char userChoice;
+        
+            Console.WriteLine("Please choose one of the following options");
+            Console.WriteLine("1 - Cappuchine");
+            Console.WriteLine("2 - Latte");
+            Console.WriteLine("3 - Americano");
+            Console.WriteLine("4 - Mocha");
+            Console.WriteLine("5 - Macchiato");
+            Console.WriteLine("6 - Expresso");
+            Console.WriteLine("Q - Quit the program");
+
+            //while (!userChoice.Equals('q'))
+            //while (!(userChoice.Equals('q') || userChoice.Equals('Q')))
+            do
+            {
+                userChoice = char.ToLower(Console.ReadKey(true).KeyChar);
+                Console.WriteLine("You chose " + userChoice);
+            } while (!userChoice.Equals('q'));
+
+        }
+    }
+}
